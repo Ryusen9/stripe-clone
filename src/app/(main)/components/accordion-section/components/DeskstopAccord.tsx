f561b16fa6530c7d97931d2e2887916cf58e2859
Ryusen9 <asratul02@gmail.com>
2026-02-03 00:44:14 +0600

    Enhance accordion components with animations and responsive design for desktop and mobile

--- src/app/(main)/components/accordion-section/components/DeskstopAccord.tsx
+++ src/app/(main)/components/accordion-section/components/DeskstopAccord.tsx
@@ -3,34 +3,73 @@ import { accordionData } from "@/data";
 import { ActionIcon, Box } from "@mantine/core";
 import { ChevronDown, Plus } from "lucide-react";
 import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 const DeskstopAccord = () => {
-  const [opened, setOpened] = useState<boolean>(false);
-  const [activeIndex, setActiveIndex] = useState<number | null>(0);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
   return (
     <Box>
       {accordionData.map((accItem, idx) => (
         <Box
-          onClick={() => {
-            setOpened(!opened);
-            setActiveIndex(idx);
-          }}
+          onClick={() => setActiveIndex(idx)}
           key={idx}
+          className="border-b border-gray-400 border-dotted cursor-pointer"
         >
-          <Box className="flex items-center justify-between">
-            <Box className="flex items-center justify-center gap-5 py-5">
-              <Image
-                src={accItem.logo.src}
-                alt={accItem.title}
-                height={40}
-                width={40}
-              />
-              <p className="text-xl font-grotesk font-medium">
-                {accItem.title}
-              </p>
+          <Box>
+            <Box className="flex items-center justify-between">
+              <Box className="flex items-center justify-center gap-5 py-5">
+                <Image
+                  src={accItem.logo.src}
+                  alt={accItem.title}
+                  height={40}
+                  width={40}
+                />
+                <p className="text-xl font-grotesk font-medium">
+                  {accItem.title}
+                </p>
+              </Box>
+              <ActionIcon>
+                {activeIndex === idx ? <ChevronDown /> : <Plus />}
+              </ActionIcon>
             </Box>
-            <ActionIcon>{opened && activeIndex === idx ? <ChevronDown /> : <Plus />}</ActionIcon>
+          </Box>
+          <Box>
+            <AnimatePresence initial={false}>
+              {activeIndex === idx && (
+                <motion.div
+                  key={`accordion-${idx}`}
+                  initial={{ height: 0, opacity: 0, y: -6 }}
+                  animate={{ height: "auto", opacity: 1, y: 0 }}
+                  exit={{ height: 0, opacity: 0, y: -6 }}
+                  transition={{ duration: 0.35, ease: "easeInOut" }}
+                  style={{ overflow: "hidden" }}
+                >
+                  <Box className="flex flex-col gap-7 py-3 transition-all duration-300">
+                    <Image
+                      src={accItem.img.src}
+                      alt={accItem.title}
+                      width={300}
+                      height={200}
+                      className="w-full h-105 object-cover rounded-2xl"
+                    />
+                    <Box className="grid grid-cols-3">
+                      {accItem.bottom.map((bottomItem, bIdx) => (
+                        <Box
+                          key={bIdx}
+                          className="flex items-start gap-1 justify-start"
+                        >
+                          <p className="font-grotesk font-semibold">
+                            {bottomItem.bold}
+                          </p>
+                          <p>{bottomItem.norm}</p>
+                        </Box>
+                      ))}
+                    </Box>
+                  </Box>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </Box>
         </Box>
       ))}
--- src/app/(main)/components/accordion-section/components/MobileAccord.tsx
+++ src/app/(main)/components/accordion-section/components/MobileAccord.tsx
@@ -1,6 +1,62 @@
+import { accordionData } from "@/data";
+import { Box } from "@mantine/core";
+import Image from "next/image";
+
 const MobileAccord = () => {
-    return (
-    <div>MobileAccord</div>
-  )
-}
-export default MobileAccord
+  return (
+    <Box className="relative">
+      {accordionData.map((accItem, idx) => (
+        <Box
+          key={idx}
+          className={`flex flex-col gap-4 pb-5 sticky bg-white ${
+            idx === 0
+              ? "top-0"
+              : idx === 1
+                ? "top-6"
+                : idx === 2
+                  ? "top-12"
+                  : ""
+          }`}
+        >
+          {/* logo and title */}
+          <Box className="flex items-center justify-start">
+            <Image
+              src={accItem.logo.src}
+              alt={accItem.title}
+              height={30}
+              width={30}
+            />
+            <p className="text-lg font-grotesk font-medium ml-4">
+              {accItem.title}
+            </p>
+          </Box>
+          {/* image */}
+          <Box>
+            <Image
+              src={accItem.img.src}
+              alt={accItem.title}
+              height={200}
+              width={300}
+              className="w-full h-60 object-cover rounded-2xl"
+            />
+          </Box>
+          {/* bottom */}
+          <Box className="grid grid-cols-1">
+            {accItem.bottom.map((bottomItem, bIdx) => (
+              <Box
+                key={bIdx}
+                className="flex items-start gap-1 justify-start mb-2"
+              >
+                <p className="text-sm font-grotesk font-bold">
+                  {bottomItem.bold}
+                </p>
+                <p className="text-sm">{bottomItem.norm}</p>
+              </Box>
+            ))}
+          </Box>
+        </Box>
+      ))}
+    </Box>
+  );
+};
+export default MobileAccord;
